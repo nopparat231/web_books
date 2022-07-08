f1bff144e92da4001c59ba02b0589283990eda4c
nopparat231 <23.noop@gmail.com>
2022-07-08 21:08:19 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
     <title>Books Pages</title>
 
     <!-- Custom fonts for this template-->
-    <link href="./vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
